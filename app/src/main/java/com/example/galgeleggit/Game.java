453e626145beb7a351b1b81732aead6bcd6149b1
niko5297 <s185082@student.dtu.dk>
2019-09-28 12:50:49 +0200

    Added new Intent for the Game Window

---
+++ app/src/main/java/com/example/galgeleggit/Game.java
@@ -0,0 +1,35 @@
+package com.example.galgeleggit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class Game extends AppCompatActivity implements View.OnClickListener {
+
+    EditText skriveFelt;
+    ImageView billede;
+    Button tjekBogstav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        tjekBogstav = findViewById(R.id.tjekBogstav);
+        skriveFelt = findViewById(R.id.skriveFelt);
+        tjekBogstav.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == tjekBogstav){
+            System.out.println(skriveFelt.getText());
+        }
+
+    }
+}
--- app/src/main/java/com/example/galgeleggit/MainActivity.java
+++ app/src/main/java/com/example/galgeleggit/MainActivity.java
@@ -2,18 +2,33 @@ package com.example.galgeleggit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView overskrift, introduktion;
     Button start;
+    ImageView billede;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.start);
+        start.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == start){
+            Intent i = new Intent(this, Game.class);
+            startActivity(i);
+        }
     }
 }
