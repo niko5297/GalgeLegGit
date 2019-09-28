f618d2b3eb86e5ed07b57da7a6aaf3f82131f40b
niko5297 <s185082@student.dtu.dk>
2019-09-28 12:04:54 +0200

    Started on landing page

--- app/src/main/java/com/example/galgeleggit/MainActivity.java
+++ app/src/main/java/com/example/galgeleggit/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.galgeleggit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView overskrift, introduktion;
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
