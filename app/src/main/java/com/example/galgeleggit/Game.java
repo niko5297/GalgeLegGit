63cb4d9da8909913bbebbc848d0f3b3596490528
nikolajwassmann <nwassmann@hotmail.com>
2019-09-30 16:57:27 +0200

    Tilføjet ikon billede

--- app/src/main/java/com/example/galgeleggit/Game.java
+++ app/src/main/java/com/example/galgeleggit/Game.java
@@ -3,6 +3,8 @@ package com.example.galgeleggit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,5 +36,21 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
             System.out.println(skriveFelt.getText());
         }
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, 1, Menu.NONE, "Hjælp");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == 1){
+            System.out.println("Du har trykket");
+        }
+
+        return true;
     }
 }
