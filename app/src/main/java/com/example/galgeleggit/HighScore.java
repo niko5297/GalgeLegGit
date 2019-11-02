7d91e6ec1574147a970f5fb36f55adebbda20f1f
niko5297 <s185082@student.dtu.dk>
2019-11-02 15:48:21 +0100

    ListView i Highscore er lavet som template.
    Toolbar tilføjet i MainActivity

--- app/src/main/java/com/example/galgeleggit/HighScore.java
+++ app/src/main/java/com/example/galgeleggit/HighScore.java
@@ -3,12 +3,32 @@ package com.example.galgeleggit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class HighScore extends AppCompatActivity {
+public class HighScore extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_high_score);
+
+        String[] lande = {"Danmark", "Norge", "Sverige", "Finland", "Holland", "Italien", "Tyskland",
+                "Frankrig", "Spanien", "Portugal", "Nepal", "Indien", "Kina", "Japan", "Thailand"};
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, lande);
+
+        ListView listView = new ListView(this);
+        listView.setOnItemClickListener(this);
+        listView.setAdapter(adapter);
+
+        setContentView(listView);
     }
+
+    public void onItemClick(AdapterView<?> liste, View v, int position, long id) {
+        Toast.makeText(this, "Klik på " + position, Toast.LENGTH_SHORT).show();
+    }
+
 }
--- app/src/main/java/com/example/galgeleggit/MainActivity.java
+++ app/src/main/java/com/example/galgeleggit/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.galgeleggit;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,6 +25,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         start = findViewById(R.id.start);
         start.setOnClickListener(this);
+
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 
     @Override
@@ -29,5 +36,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent i = new Intent(this, Game.class);
             startActivity(i);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.game_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id){
+            case R.id.hjælp:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+                dialog.setTitle("Hjælp");
+                dialog.setIcon(R.drawable.ic_help_black_24dp);
+                dialog.setMessage("Spillet går ud på at du skal gætte det ord som maskinen tænker på. \n" +
+                        "Dette gøres ved at skrive et bogstav. For hvert rigtigt svar vises det bogstav i ordet. " +
+                        "For hvert forkert svar tegnes noget af galgen. Hele galgen vil være tegnet ved 6 forkerte gæt. \n" +
+                        "Det gælder om at gætte hele ordet før galgen er blevet tegnet. \n\n" +
+                        "Held og lykke.");
+                dialog.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                    }
+                });
+                dialog.show();
+                break;
+            case R.id.highscore:
+                Intent i = new Intent(this,HighScore.class);
+                startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
